58d03a7b1a3c8969a009dbd166ce40f9d7823476
LenaVorontsova <lena.voroncova26@yandex.ru>
2021-12-16 09:54:56 +0400

    fix: change the name of the login button to register, add: configure the transition to the registration window by clicking on the button

--- app/src/main/java/com/example/invsb/AuthorizationActivity.java
+++ app/src/main/java/com/example/invsb/AuthorizationActivity.java
@@ -1,7 +1,11 @@
 package com.example.invsb;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AuthorizationActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class AuthorizationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authorization);
+
+        Button buttonLogin = (Button)findViewById(R.id.loginButton);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(AuthorizationActivity.this,RegistrationActivity.class);
+                    startActivity(intent);finish();
+                }
+                catch (Exception e) {
+
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/invsb/RegistrationActivity.java
+++ app/src/main/java/com/example/invsb/RegistrationActivity.java
@@ -3,6 +3,8 @@ package com.example.invsb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class RegistrationActivity extends AppCompatActivity {
 
