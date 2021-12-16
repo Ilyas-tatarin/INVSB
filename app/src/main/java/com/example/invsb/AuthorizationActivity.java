9c365ecbb66f2ecc2f684aa644c0d555a82ed9de
LenaVorontsova <lena.voroncova26@yandex.ru>
2021-12-16 12:00:07 +0400

    add: add the firebase

--- app/src/main/java/com/example/invsb/AuthorizationActivity.java
+++ app/src/main/java/com/example/invsb/AuthorizationActivity.java
@@ -1,13 +1,24 @@
 package com.example.invsb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthorizationActivity extends AppCompatActivity {
+    private EditText emailLogin;
+    private EditText passLogin;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +29,36 @@ public class AuthorizationActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    Intent intent = new Intent(AuthorizationActivity.this,RegistrationActivity.class);
-                    startActivity(intent);finish();
-                }
-                catch (Exception e) {
+                Intent intent = new Intent(AuthorizationActivity.this, RegistrationActivity.class);
+                startActivity(intent);finish();
+            }
+        });
 
+        emailLogin = findViewById(R.id.emailText);
+        passLogin = findViewById(R.id.passText);
+
+        mAuth = FirebaseAuth.getInstance();
+        Button enterButton = (Button)findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (emailLogin.getText().toString().isEmpty() || passLogin.getText().toString().isEmpty()){
+                    Toast.makeText(AuthorizationActivity.this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.signInWithEmailAndPassword(emailLogin.getText().toString(), passLogin.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()) {
+                                        Intent intent = new Intent(AuthorizationActivity.this, MainActivity.class);
+                                        startActivity(intent); finish();
+                                    }
+                                    else {
+                                        Toast.makeText(AuthorizationActivity.this, "Ошибка", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
             }
         });
--- app/src/main/java/com/example/invsb/RegistrationActivity.java
+++ app/src/main/java/com/example/invsb/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.invsb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,19 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private EditText emailReg;
+    private EditText passReg;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +32,37 @@ public class RegistrationActivity extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    Intent intent = new Intent(RegistrationActivity.this,AuthorizationActivity.class);
-                    startActivity(intent);finish();
-                }
-                catch (Exception e) {
+                Intent intent = new Intent(RegistrationActivity.this,AuthorizationActivity.class);
+                startActivity(intent);finish();
+            }
+        });
 
+        emailReg = findViewById(R.id.emailRegText);
+        passReg = findViewById(R.id.passRegText);
+        mAuth = FirebaseAuth.getInstance();
+        Button saveButton = (Button)findViewById(R.id.enterButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (emailReg.getText().toString().isEmpty() || passReg.getText().toString().isEmpty()){
+                    Toast.makeText(RegistrationActivity.this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.createUserWithEmailAndPassword(emailReg.getText().toString(), passReg.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                                startActivity(intent); finish();
+                            }
+                            else {
+                                Toast.makeText(RegistrationActivity.this, "Ошибка", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
+
     }
 }
