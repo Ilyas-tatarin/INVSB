b31992a9fe385db5bafc43f33caf23debfdc6652
LenaVorontsova <lena.voroncova26@yandex.ru>
2021-12-16 10:05:35 +0400

    add: add a transition from the registration window to the authorization window

--- app/src/main/java/com/example/invsb/RegistrationActivity.java
+++ app/src/main/java/com/example/invsb/RegistrationActivity.java
@@ -2,9 +2,12 @@ package com.example.invsb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -12,5 +15,19 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Button backButton = (Button)findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(RegistrationActivity.this,AuthorizationActivity.class);
+                    startActivity(intent);finish();
+                }
+                catch (Exception e) {
+
+                }
+            }
+        });
     }
 }
