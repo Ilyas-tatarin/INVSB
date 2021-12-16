c440ad82aeaf9be7d74a7a501dfe5d3efed65483
irkarimov <ibnrinat02@gmail.com>
2021-12-17 01:33:36 +0400

    create a link for course

--- app/src/main/java/com/example/invsb/Tinkoff.java
+++ app/src/main/java/com/example/invsb/Tinkoff.java
@@ -2,13 +2,29 @@ package com.example.invsb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Tinkoff extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tinkoff);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new
+                        Intent(Intent.ACTION_VIEW, Uri.parse("https://journal.tinkoff.ru/pro/stonks/"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
