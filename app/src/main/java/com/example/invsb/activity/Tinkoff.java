a4892f9bc4afab6664ca3c26070ab0dd4e35e2b0
Lev Gromov <levgromov667@gmail.com>
2021-12-28 02:46:37 -0800

    docs

--- app/src/main/java/com/example/invsb/activity/Tinkoff.java
+++ app/src/main/java/com/example/invsb/activity/Tinkoff.java
@@ -10,10 +10,26 @@ import android.widget.Button;
 
 import com.example.invsb.R;
 
+/**
+ * Класс, реализующий переход к курсам от Тинькофф банка
+ *              со свойствами <b>button</b> и <b>button1</b>
+ * @author Юлия Тарасова
+ * @version 0.1
+ *
+ * */
 public class Tinkoff extends AppCompatActivity {
+
+    /** Поле, иницилизирующее кнопку*/
     Button button;
+    /** Поле, иницилизирующее вторую кнопку*/
     Button button2;
 
+    /**
+     * Функция, инициализирующая
+     *              пользовтельский интерфейс в виде кнопок для перехода к курсам
+     *
+     * @param savedInstanceState - сохраненное состояние
+     * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
